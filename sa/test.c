bcea9bda3d20869437d21e8072627edc109ca26d
Yicheng Zou <zyc1995@mail.ustc.edu.cn>
2016-11-30 16:49:08 +0800

    Complete basic requirements of csa reading mission, leaving analysis.txt to be edited.

---
+++ sa/test.c
@@ -0,0 +1,16 @@
+void test()
+{
+    int i;
+    int temp;
+    for(i = 1; i<100; i++)
+    {
+        if(i<50)
+        {
+            temp = 1;
+        }
+        else
+        {
+            temp = 0;
+        }
+    }
+}
